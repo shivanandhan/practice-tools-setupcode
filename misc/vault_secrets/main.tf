949c644d9fbb66bba793bc67142191a723a287a8
Shiva <shivanandhan33@gmail.com>
2024-12-26 21:03:48 +0530

    New repo

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -18,22 +18,22 @@ resource "vault_mount" "roboshop-dev" {
   type        = "kv"
   options     = { version = "2" }
   description = "Roboshop dev secrets"
-}
+ }
 
 resource "vault_generic_secret" "frontend" {
   path = "${vault_mount.roboshop-dev.path}/frontend"
 
   data_json = <<EOT
-{
+ {
   "catalogue_url": "http://catalogue-dev.kndevops72.online/",
   "cart_url":    "http://cart-dev.kndevops72.online/",
   "user_url":    "http://user-dev.kndevops72.online/",
   "shipping_url": "http://shipping-dev.kndevops72.online/",
   "payment_url":  "http://payment-dev.kndevops72.online/"
 
-}
+ }
 EOT
-}
+ }
 
 resource "vault_generic_secret" "catalogue" {
   path = "${vault_mount.roboshop-dev.path}/catalogue"
