be87428f8cfd9aeff0edbb35b4dbcaa2b0ad6e55
Shiva <shivanandhan33@gmail.com>
2024-12-31 21:46:02 +0530

    New repo

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -43,11 +43,11 @@ resource "vault_generic_secret" "frontend" {
 
   data_json = <<EOT
  {
-  "catalogue_url": "http://catalogue-dev.kndevops72.online/",
-  "cart_url":    "http://cart-dev.kndevops72.online/",
-  "user_url":    "http://user-dev.kndevops72.online/",
-  "shipping_url": "http://shipping-dev.kndevops72.online/",
-  "payment_url":  "http://payment-dev.kndevops72.online/"
+  "catalogue_url": "http://catalogue-dev.kndevops72.online:8080/",
+  "cart_url":    "http://cart-dev.kndevops72.online:8080/",
+  "user_url":    "http://user-dev.kndevops72.online:8080/",
+  "shipping_url": "http://shipping-dev.kndevops72.online:8080/",
+  "payment_url":  "http://payment-dev.kndevops72.online:8080/"
 
  }
 EOT
@@ -57,24 +57,24 @@ resource "vault_generic_secret" "catalogue" {
   path = "${vault_mount.roboshop-dev.path}/catalogue"
 
   data_json = <<EOT
-{
+ {
  "MONGO": "true",
  "MONGO_URL" : "mongodb://mongodb-dev.kndevops72.online:27017/catalogue"
-}
+ }
 EOT
-}
+ }
 
 resource "vault_generic_secret" "user" {
   path = "${vault_mount.roboshop-dev.path}/user"
 
   data_json = <<EOT
-{
- "MONGO" : "true",
- "REDIS_URL" : "redis://redis-dev.kndevops72.online:6379",
-  "MONGO_URL": "mongodb://mongodb-dev.kndevops72.online:27017/users"
-}
+ {
+  "MONGO" : "true",
+  "MONGO_URL": "mongodb://mongodb-dev.kndevops72.online:27017/users",
+  "REDIS_URL" : "redis://redis-dev.kndevops72.online:6379"
+ }
 EOT
-}
+ }
 
 resource "vault_generic_secret" "cart" {
   path = "${vault_mount.roboshop-dev.path}/cart"
@@ -92,19 +92,19 @@ resource "vault_generic_secret" "shipping" {
   path = "${vault_mount.roboshop-dev.path}/shipping"
 
   data_json = <<EOT
-{
- "CART_ENDPOINT" : "cart-dev.kndevops72.online:8080",
+  {
+  "CART_ENDPOINT": "cart-dev.kndevops72.online:8080",
   "DB_HOST" : "mysql-dev.kndevops72.online",
   "mysql_root_password" : "Roboshop@1"
-}
+  }
 EOT
-}
+  }
 
 resource "vault_generic_secret" "payment" {
   path = "${vault_mount.roboshop-dev.path}/payment"
 
   data_json = <<EOT
-{
+ {
  "CART_HOST" : "cart-dev.kndevops72.online",
  "CART_PORT" : "8080",
  "USER_HOST" : "user-dev.kndevops72.online",
@@ -112,27 +112,27 @@ resource "vault_generic_secret" "payment" {
  "AMQP_HOST" : "rabbitmq-dev.kndevops72.online",
  "AMQP_USER" : "roboshop",
  "AMQP_PASS" : "roboshop123"
-}
+ }
 EOT
-}
+ }
 
 resource "vault_generic_secret" "mysql" {
   path = "${vault_mount.roboshop-dev.path}/mysql"
 
   data_json = <<EOT
-{
+ {
   "mysql_root_password" : "Roboshop@1"
-}
+ }
 EOT
-}
+ }
 
 resource "vault_generic_secret" "rabbitmq" {
   path = "${vault_mount.roboshop-dev.path}/rabbitmq"
 
   data_json = <<EOT
-{
+ {
  "user" : "roboshop",
  "password" : "roboshop123"
-}
+ }
 EOT
-}
+ }
