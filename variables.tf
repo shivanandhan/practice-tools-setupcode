427e2e5b1bad0e206525faeea45bf353c7f9c46f
Shiva <shivanandhan33@gmail.com>
2025-01-07 12:02:33 +0530

    New repo

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "tools" {
       port        = 8200
       volume_size = 20
       instance_type = "t3.small"
-      policy_list = ["ec2:DescribeKeyPairs"]
+      policy_list = []
     }
 
     github-runner = {
