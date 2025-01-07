f709e1defa3273c384abc51386caae180eb901ce
Shiva <shivanandhan33@gmail.com>
2025-01-07 12:20:35 +0530

    New repo

--- module-ec2/iam.tf
+++ module-ec2/iam.tf
@@ -39,6 +39,7 @@ resource "aws_iam_role" "role" {
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
+  count = length(var.policy_list) > 0 ? 1: 0
   name = "${var.tool_name}-role"
-  role = aws_iam_role.role.name
+  role = aws_iam_role.role[0].name
 }
